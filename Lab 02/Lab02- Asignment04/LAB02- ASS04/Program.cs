2d681ac020267570ddcb2cf6708c1210013e1029
esraa <89482439+E2497@users.noreply.github.com>
2024-07-19 20:16:09 +0300

    Add files via upload
    C# Basics

---
+++ Lab 02/Lab02- Asignment04/LAB02- ASS04/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        int rowsNum = GetValueFromUser("Enter number of rows ");
+        int[][] arr = new int[rowsNum][];       
+            
+
+
+        for (int i = 1; i <= rowsNum; i++)
+        {
+
+            int ColNum = GetValueFromUser("Enter No of Columns in Row # " + i + " ");
+            arr[i - 1] = new int [ColNum];
+            Console.WriteLine(ColNum);
+
+            for (int j = 1; j <= ColNum; j++)
+            {
+                arr[i - 1][j-1] =  GetValueFromUser("Row # " + i + " column # " + j +" ");
+            }
+        }
+        for (int i = 1; i <= rowsNum; i++)
+        {
+
+            for (int j = 1; j <= arr[i-1].Length; j++)
+            {
+                Console.WriteLine ("row # {0} and column {1} #",i,j,arr[i-1][j-1]);
+               
+            }
+        }
+
+
+        static int GetValueFromUser(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
