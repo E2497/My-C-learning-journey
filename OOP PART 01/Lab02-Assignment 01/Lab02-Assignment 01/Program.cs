3a4e4c34f3c586413803294683f4ed458a0d30c9
esraa <89482439+E2497@users.noreply.github.com>
2024-09-13 12:20:03 +0300

    Add files via upload

---
+++ OOP PART 01/Lab02-Assignment 01/Lab02-Assignment 01/Program.cs
@@ -0,0 +1,81 @@
+﻿class Program
+{
+    static void Main(string[] args)
+
+    {
+       
+        Employee[] e = new Employee[5];
+        Employee e1 = new Employee("E1");
+        Employee e2 = new Employee("E2");
+        Employee e3 = new Employee("E3");
+        e1.PrintInfo();
+        e2.PrintInfo();
+        e3.PrintInfo();
+        e[0] = new Employee("E4");
+        e.PrintInfoForArray(0);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    static int GetValueFromUser(string message)
+    {
+        Console.Write(message);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+
+
+
+    
+}
+public class Employee
+{
+    public string name;
+    public int id;
+    public static int automaticId = 0;
+
+    public Employee(string name)
+    {
+        this.name = name;
+        automaticId++;
+        this.id = automaticId;
+
+    }
+
+    public void PrintInfo()
+    {
+
+        
+        // h.SetConsoleColor((Color)e[i].favColor);
+        Console.WriteLine($"id:{id}");
+        Console.WriteLine($"name:{name}");
+        Console.ReadLine();
+        Console.Clear();
+
+    }
+}
+static class ExtensionClass
+{
+    public static void PrintInfoForArray(this Employee [] e,int index)
+    {
+        Console.WriteLine($"id:{e[index].id}");
+        Console.WriteLine($"name:{e[index].name}");
+        Console.ReadLine();
+       
+    }
+
+}
+
+
+
+
+
---
+++ OOP PART 01/Lab03 - Assignment 01/Lab03 - Assignment 01/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        Employee e = new Employee("e", 24, 500);
+        
+        Console.WriteLine(e.ToString());
+    }
+}
+
+public class Human
+{
+    public string name;
+    public int age;
+
+
+    public Human (string name, int age ) {
+        this.name = name; 
+        this.age = age;
+    }
+
+
+
+ }
+
+public class Employee : Human
+{   int salary;
+    public Employee(string name , int age, int salary) : base (name,age)
+    {
+        this.salary = salary;   
+    }
+    public override string ToString()
+
+    {
+        string x = this.name;
+       
+        return  $"Name:{this.name}, Salary:{this.salary}" ;
+    }
+}
+
+    
+
+
